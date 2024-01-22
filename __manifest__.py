004bd5de7450026e54660bbf125fcda8ee8c042f
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-01-22 10:18:20 +0100

    Unpaid invoices:Add the field «Customer reference»

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Account customer followup',
-    'version': '1.0.1.6',
+    'version': '1.0.1.7',
     'author':'Soft-integration',
     'category': 'Accounting',
     'description': "",
