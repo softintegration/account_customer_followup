4b067af0e774b444dd5dbabc617a8d0daefe68d6
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-03-17 22:09:02 +0100

    Payment reminder:Display only Company Customers

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Account customer followup',
-    'version': '1.0.1.15',
+    'version': '1.0.1.16',
     'author':'Soft-integration',
     'category': 'Accounting',
     'description': "",
